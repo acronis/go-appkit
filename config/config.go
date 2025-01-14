ade3906d56f08013c5a7262a049c0888eca9951e
Ghislain Rodrigues <ghislain.rodrigues@acronis.com>
2025-01-14 11:18:15 +0100

    Skip unexported fields in CallSetProviderDefaultsForFields and CallSetForFields.
    Until now, the sub structs of a configuration were expected to have only
    exported (public) fields, With this change, a configuration can be
    composed of nested configurations which can themselves call
    CallSetProviderDefaultsForFields and CallSetForFields and have
    unexported fields (e.g. a `keyPrefix`).

--- config/config.go
+++ config/config.go
@@ -24,6 +24,9 @@ type KeyPrefixProvider interface {
 func CallSetProviderDefaultsForFields(obj interface{}, dp DataProvider) {
 	el := reflect.ValueOf(obj).Elem()
 	for i := 0; i < el.NumField(); i++ {
+		if !el.Type().Field(i).IsExported() {
+			continue
+		}
 		v := el.Field(i).Interface()
 		if reflect.ValueOf(v).Kind() == reflect.Ptr && reflect.ValueOf(v).IsNil() {
 			continue
@@ -43,6 +46,9 @@ func CallSetProviderDefaultsForFields(obj interface{}, dp DataProvider) {
 func CallSetForFields(obj interface{}, dp DataProvider) error {
 	el := reflect.ValueOf(obj).Elem()
 	for i := 0; i < el.NumField(); i++ {
+		if !el.Type().Field(i).IsExported() {
+			continue
+		}
 		v := el.Field(i).Interface()
 		if reflect.ValueOf(v).Kind() == reflect.Ptr && reflect.ValueOf(v).IsNil() {
 			continue
