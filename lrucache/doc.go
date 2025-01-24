2d958af8db67abfb97fa7213403537be7e6dcef4
Vasily Tsybenko <vasayxtx@gmail.com>
2025-01-24 12:14:39 +0200

    Implement expiration mechanism support in lrucache package

--- lrucache/doc.go
+++ lrucache/doc.go
@@ -4,5 +4,5 @@ Copyright © 2024 Acronis International GmbH.
 Released under MIT license.
 */
 
-// Package lrucache provides in-memory cache with LRU eviction policy and Prometheus metrics.
+// Package lrucache provides in-memory cache with LRU eviction policy, expiration mechanism, and Prometheus metrics.
 package lrucache
