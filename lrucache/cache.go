c6995c9b86e9b056c9888e132c341c8aa140776d
Vasily Tsybenko <vasayxtx@gmail.com>
2025-02-21 10:03:05 +0200

    Implement GetOrLoad() using singleflight pattern in lrucache

--- lrucache/cache.go
+++ lrucache/cache.go
@@ -20,6 +20,11 @@ type cacheEntry[K comparable, V any] struct {
 	expiresAt time.Time
 }
 
+type singleFlightCallResult[V any] struct {
+	value  V
+	exists bool
+}
+
 // LRUCache represents an LRU cache with eviction mechanism and Prometheus metrics.
 type LRUCache[K comparable, V any] struct {
 	maxEntries int
@@ -29,6 +34,8 @@ type LRUCache[K comparable, V any] struct {
 	mu      sync.RWMutex
 	lruList *list.List
 	cache   map[K]*list.Element // map of cache entries, value is a lruList element
+
+	sfGroup *singleFlightGroup[K, singleFlightCallResult[V]]
 
 	metricsCollector MetricsCollector
 }
@@ -64,6 +71,7 @@ func NewWithOpts[K comparable, V any](maxEntries int, metricsCollector MetricsCo
 		maxEntries:       maxEntries,
 		lruList:          list.New(),
 		cache:            make(map[K]*list.Element),
+		sfGroup:          &singleFlightGroup[K, singleFlightCallResult[V]]{},
 		metricsCollector: metricsCollector,
 		defaultTTL:       opts.DefaultTTL,
 	}, nil
@@ -103,16 +111,22 @@ func (c *LRUCache[K, V]) AddWithTTL(key K, value V, ttl time.Duration) {
 	c.addNew(key, value, expiresAt)
 }
 
-// GetOrAdd returns a value from the cache by the provided key.
-// If the key does not exist, it adds a new value to the cache.
+// GetOrAdd returns a value from the cache by the provided key,
+// and adds a new value with the default TTL if the key does not exist.
+// The new value is provided by the valueProvider function.
+// The function is called only if the key does not exist.
+// Note that the function is called under the LRUCache lock, so it should be fast and non-blocking.
+// If you need to perform a blocking operation, consider using GetOrLoad instead.
 func (c *LRUCache[K, V]) GetOrAdd(key K, valueProvider func() V) (value V, exists bool) {
 	return c.GetOrAddWithTTL(key, valueProvider, c.defaultTTL)
 }
 
-// GetOrAddWithTTL returns a value from the cache by the provided key.
-// If the key does not exist, it adds a new value to the cache with the provided TTL.
-// Please note that expired entries are not removed immediately,
-// but only when they are accessed or during periodic cleanup (see RunPeriodicCleanup).
+// GetOrAddWithTTL returns a value from the cache by the provided key,
+// and adds a new value with the specified TTL if the key does not exist.
+// The new value is provided by the valueProvider function.
+// The function is called only if the key does not exist.
+// Note that the function is called under the LRUCache lock, so it should be fast and non-blocking.
+// If you need to perform a blocking operation, consider using GetOrLoad instead.
 func (c *LRUCache[K, V]) GetOrAddWithTTL(key K, valueProvider func() V, ttl time.Duration) (value V, exists bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -128,6 +142,38 @@ func (c *LRUCache[K, V]) GetOrAddWithTTL(key K, valueProvider func() V, ttl time
 	value = valueProvider()
 	c.addNew(key, value, expiresAt)
 	return value, false
+}
+
+// GetOrLoad returns a value from the cache by the provided key,
+// and loads a new value if the key does not exist.
+// The new value is provided by the loadValue function.
+// The function is called only if the key does not exist.
+// Note that the single flight pattern is used to prevent multiple concurrent calls for the same key.
+func (c *LRUCache[K, V]) GetOrLoad(
+	key K, loadValue func(K) (value V, ttl time.Duration, err error),
+) (value V, exists bool, err error) {
+	if val, ok := c.Get(key); ok {
+		return val, true, nil
+	}
+
+	result, doErr := c.sfGroup.Do(key, func() (singleFlightCallResult[V], error) {
+		if val, ok := c.Get(key); ok { // double check after possible concurrent call
+			return singleFlightCallResult[V]{value: val, exists: true}, nil
+		}
+		val, ttl, valErr := loadValue(key)
+		if valErr != nil {
+			return singleFlightCallResult[V]{value: val, exists: false}, valErr
+		}
+		if ttl <= 0 {
+			ttl = c.defaultTTL
+		}
+		c.AddWithTTL(key, val, ttl)
+		return singleFlightCallResult[V]{value: val, exists: false}, nil
+	})
+	if doErr != nil {
+		return value, false, doErr
+	}
+	return result.value, result.exists, nil
 }
 
 // Remove removes a value from the cache by the provided key and type.
