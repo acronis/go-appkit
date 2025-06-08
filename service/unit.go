08f1e17beb19d3ac47b69300ecb2654901e54229
Oleg.Rudenko <Oleg.Rudenko@acronis.com>
2025-06-08 18:57:32 +0300

    Fix race condition in `CompositeUnit.Start` method, PLTFRM-78429

--- service/unit.go
+++ service/unit.go
@@ -6,9 +6,24 @@ Released under MIT license.
 
 package service
 
-// Unit is a common interface for all service units. Unit is a single component in service with its own life-cycle.
+// Unit represents a service unit that can be started and stopped.
+// Each Unit is a distinct component within a service, with its own lifecycle.
 type Unit interface {
-	Start(fatalError chan<- error)
+	// Start begins the unit's operation.
+	//
+	// On the happy path, an implementation may perform necessary initialization and return immediately,
+	// or block the calling goroutine for the duration of the unit's lifetime.
+	// The Stop method may be called regardless of whether the unit has started successfully, failed, or
+	// is still running.
+	//
+	// If Start succeeds, it must not write anything to the provided error channel.
+	// Additionally, the channel must not be used after the Start method has returned.
+	Start(fatalErr chan<- error)
+
+	// Stop halts the unit.
+	//
+	// If 'gracefully' is true, the unit should attempt a clean shutdown.
+	// Note that this method may be called even if Start has failed or was never called.
 	Stop(gracefully bool) error
 }
 
